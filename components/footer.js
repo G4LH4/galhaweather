114b4e7785d0bebec3e986b04e02d194d3106785
G4LH4 <totogalha@gmail.com>
2022-05-22 23:21:54 -0300

    Add menu bar. Add hooks. Change next config

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,28 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import horizontalBar from "../public/horizontal-bar.png";
+
 const Footer = () => {
-  return <section id="footer"></section>;
+  return (
+    <motion.div
+      id="footer"
+      className="w-full mt-auto align-middle bg-boxColor"
+      drag="y"
+      dragConstraints={{
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}
+      dragElastic={0.6}
+      // onDragEnd={(event, info) => console.log(info.point.x, info.point.y)}
+    >
+      <div className="mx-auto w-fit">
+        <Image src={horizontalBar} />
+      </div>
+    </motion.div>
+  );
 };
 
 export default Footer;
